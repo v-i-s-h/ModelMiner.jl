7c3a34c91b245a7baa7d3bc70d00f419b430a89f
v-i-s-h <get_vichu@yahoo.com>
2023-01-23 18:25:00 +0200

    Tests - initial

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,15 @@
 using ModelMiner
 using Test
+using RDatasets
 
 @testset "ModelMiner.jl" begin
-    # Write your tests here.
+    data = dataset("datasets", "iris")
+    
+    # Split into features and targets
+    y, X = unpack(data, ==(:Species))
+    
+    a = mine(X, y)
+    print(a)
+
+    @test true # Dummy test to return pass.
 end
