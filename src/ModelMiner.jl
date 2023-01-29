0fbad12f0971fe7377c8739b2c97fd474cd4d3bd
v-i-s-h <get_vichu@yahoo.com>
2023-01-29 09:08:53 +0200

    Models update

--- src/ModelMiner.jl
+++ src/ModelMiner.jl
@@ -1,6 +1,7 @@
 module ModelMiner
 import MLJ
 import MLJModels
+import DataFrames: DataFrame
 
 
 """
@@ -11,8 +12,7 @@ Load avaliable packages from a NamedTuple of algorithms.
 in `algos` is that each entry should have `name` and `package_name`.
 
 Note: This function will try to load all given algorithms if it is available
-on the current environment. The user is required to keep the environment updated
-with the packages they want to train the models with.
+on the packages environment.
 """
 function _load_mlj_packages(algos)
     # Find all MLJPackages
@@ -27,11 +27,9 @@ function _load_mlj_packages(algos)
         try
             eval(:(import $(Symbol(pkg_name))))
             push!(loaded_models, algo_path)
-            # @info "Loaded " * algo_path
         catch e
-            if isa(e, ArgumentError)
-                # @warn "Skipping " * algo_path
-            else
+            if !isa(e, ArgumentError)
+                # If the error is not related to importing the MLJ Modules
                 rethrow()
             end
         end
@@ -43,6 +41,10 @@ end
 # Load all available packages
 const LOADED_MODELS = _load_mlj_packages(MLJ.models())
 
+const BLACKLIST_MODELS = [
+    (name="MultitargetNeuralNetwork", package_name="BetaML")
+]
+
 
 """
     mine()
@@ -52,7 +54,9 @@ Train all avaliable models from the environment.
 function _mine(data...; measures=[])
     match_fn = MLJ.matching(data...)
     available_algos = MLJ.models(
-        m -> match_fn(m) && (MLJModels.load_path(m) ∈ LOADED_MODELS)
+        m -> match_fn(m) 
+        && (MLJModels.load_path(m) ∈ LOADED_MODELS)
+        && (m.name ∉ [bl.name for bl in BLACKLIST_MODELS])
     )
 
 
@@ -60,29 +64,34 @@ function _mine(data...; measures=[])
 
     for _a ∈ available_algos
         _model = MLJModels.load_path(_a)
-        # @info "Evaluating " * _model
         model = _model |> Meta.parse |> eval
         machine = model()
 
-        # Train and evaluate
-        r = MLJ.evaluate(
-            machine,
-            data...;
-            resampling=MLJ.CV(shuffle=true),
-            verbosity=0,
-            measures=measures
-        )
-
-        # Save results
-        push!(
-            results, (
-                name=_a.name,
-                NamedTuple(zip(
-                    measures .|> typeof .|> t -> t.name.name, # get measurement name as symbol
-                    r.measurement # corresponding measurement value
-                ))...
+        try
+            # Train and evaluate
+            r = MLJ.evaluate(
+                machine,
+                data...;
+                resampling=MLJ.CV(shuffle=true),
+                verbosity=0,
+                measures=measures
             )
-        )
+
+            # Save results
+            push!(
+                results, (
+                    name=_a.name,
+                    NamedTuple(zip(
+                        measures .|> typeof .|> t -> t.name.name, # get measurement name as symbol
+                        r.measurement # corresponding measurement value
+                    ))...
+                )
+            )
+        catch
+            # Some error happened in training this model.
+            # Let's skip this
+            # @warn "Skipping $(_a.name) ($(_a.package_name)) due to internal errors"
+        end
     end
 
     return results
@@ -94,17 +103,17 @@ end
 Train regression models (where target `y` is a real value).
 """
 function mine(X, y::Vector{T}) where {T<:AbstractFloat}
-    return _mine(X, y; measures=[MLJ.rms])
+    return _mine(X, y; measures=[MLJ.rms]) |> DataFrame
 end
 
 """
     mine(X, y)
 
-Train classification models.
-(because regression models are handled above?)
+Train non-regression (classification) models (where `y` is not a floating point
+variable).
 """
 function mine(data...)
-    return _mine(data...; measures=[MLJ.accuracy, MLJ.multiclass_f1score])
+    return _mine(data...; measures=[MLJ.accuracy, MLJ.multiclass_f1score]) |> DataFrame
 end
 
 export mine
